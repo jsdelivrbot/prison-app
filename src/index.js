d6594904da2ebbf1bd414ba2ede5e752cf8f6071
Mar <marwanest@live.fr>
2018-08-01 09:20:06 -0500

    implementing mongo database

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import MenuAppBar from './MenuAppBar';
+import {
+   Stitch,
+   RemoteMongoClient,
+   GoogleRedirectCredential,
+   FacebookRedirectCredential
+} from "mongodb-stitch-browser-sdk";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
+
+let appId = "first_application-xnjws";
