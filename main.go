c5844f7b2e6a113c84ea5a0440ee96c56e7eb4fb
Mar <marwanest@live.fr>
2018-08-06 11:16:35 -0500

    shorter way

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		f, err := os.Open("public" + r.URL.Path)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Println(err)
-		}
-		defer f.Close()
-		io.Copy(w, f)
+		http.ServeFile(w, r, "public"+r.URL.Path)
 	})
 	http.ListenAndServe(":8000", nil)
 }
