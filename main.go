3f012b89bff9efc31392d74deb1f7ed9c8ecd2db
Mar <marwanest@live.fr>
2018-08-06 13:40:06 -0500

    implementing template

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer((http.Dir("templates"))))
+	templates := populateTemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]
+		template := templates.Lookup(requestedFile + ".html")
+		if template != nil {
+			err := template.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
