33cfcb33e1dba98ab043f96a373c8d6837367e47
Mar <marwanest@live.fr>
2018-08-11 00:35:20 -0500

    Reconnected React to Stitch successfully.
    Able to access my cluster.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
 import './App.css';
 import MenuAppBar from './MenuAppBar';
 import EnhancedTableHead from './Table';
 import SimpleModal from './Modal';
-import { Stitch } from 'mongodb-stitch-browser-sdk'
-// import FacebookLogin from './FacebookLogin';
 
-var MongoClient = require('mongodb').MongoClient;
-
-var uri = "mongodb+srv://marwan01:motdepasse@my-first-cluster-hzz9b.gcp.mongodb.net/First_Database?retryWrites=true";
-MongoClient.connect(uri, function(err, client) {
-   const collection = client.db("First_Database").collection("First_Collection");
-   // perform actions on the collection object
-   client.close();
-});
 
 class App extends Component {
   render() {
